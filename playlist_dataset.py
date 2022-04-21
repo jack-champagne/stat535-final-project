3e6a3bf3a270c85b88e069e70bee71a6174f1eff
jack-champagne <jackchampagne.r@gmail.com>
2022-04-21 01:15:57 -0400

    add changes to dataloader

--- playlist_dataset.py
+++ playlist_dataset.py
@@ -7,9 +7,9 @@ top_song_count = 2000
 
 class TopSongsTrain(Dataset):
     def __init__(self, file_name):
-        pl_top_trk_df = pd.read_csv()
+        pl_top_trk_df = pd.read_csv(file_name, names=['pid'] + [i for i in range(0, top_song_count)])
 
-        x=pl_top_trk_df[:,0:top_song_count].values
+        x=pl_top_trk_df[[i for i in range(0, top_song_count)]].values
         y=pl_top_trk_df[:top_song_count].values
 
         self.x_train=torch.tensor(x, dtype=torch.bool)
