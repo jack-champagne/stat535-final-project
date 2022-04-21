30972b8e9d1ece5e3666fd4c3f86d469311bbb08
Jack Champagne <jackchampagne.r@gmail.com>
2022-04-20 23:36:28 -0400

    Pre training commit

--- playlist_dataset.py
+++ playlist_dataset.py
@@ -7,7 +7,7 @@ top_song_count = 2000
 
 class TopSongsTrain(Dataset):
     def __init__(self, file_name):
-        pl_top_trk_df = pd.read_json()
+        pl_top_trk_df = pd.read_csv()
 
         x=pl_top_trk_df[:,0:top_song_count].values
         y=pl_top_trk_df[:top_song_count].values
@@ -18,5 +18,5 @@ class TopSongsTrain(Dataset):
         def __len__(self):
             return len(self.y_train)
 
-        def __getitem(self, idx):
+        def __getitem__(self, idx):
             return self.x_train[idx], self.y_train[idx]
